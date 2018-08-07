92391f5532eb14d7928f060b9d2ee3f349ff4f65
Jim Roberts <jamesjroberts@gmail.com>
2018-08-07 14:24:27 +0100

    Fixed line 6: Changed context -> context.aapl
    Changed context -> context.aapl

--- algo.py
+++ algo.py
@@ -3,7 +3,7 @@ import pandas
 def initialize(context): #alleviates need to call functions
     context.aapl=sid(24) #aapl=company ticker. sid=stock ID. tickers can change over time from being bought/sold, companies closing down etc. sid used to ensure investment in desired company
 def handle_data(context, data): #function takes data; universe of information. contains everything, eg stock prices
-    hist=data.history(context, 'price', 50, '1d') #50 periods of 1 day data history prices for Apple
+    hist=data.history(context.aapl, 'price', 50, '1d') #50 periods of 1 day data history prices for Apple
     log.info(hist.head())#uses pandas data
     sma_50=hist.mean() #sma is simple moving average, hist takes it from the above defined data set
     sma_20=hist[-20:].mean() #mean of the last 20 days of the history
